6c59e836ad10c8a3198e6cdd27731ec75ef5a3e2
simaypay <simaypay@gmail.com>
2025-05-31 19:04:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,15 +32,13 @@ def angles_finder(landmarks):
     right_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
                                       landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value],
                                       landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
-    angle_for_ardhaChandrasana1 = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value],
+    angle_for_half_moon1 = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value],
                                                  landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
                                                  landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value])
-    angle_for_ardhaChandrasana2 = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value],
+    angle_for_half_moon2 = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value],
                                                  landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
                                                  landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
-    hand_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
-                                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
-                                landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value])
+    
     left_hip_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
                                     landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
                                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value])
@@ -57,33 +55,26 @@ def angles_finder(landmarks):
                                           landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
                                           landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
     return [left_elbow_angle, right_elbow_angle, left_shoulder_angle, right_shoulder_angle,
-            left_knee_angle, right_knee_angle, angle_for_ardhaChandrasana1, angle_for_ardhaChandrasana2,
-            hand_angle, left_hip_angle, right_hip_angle, neck_angle_uk, left_wrist_angle_bk, right_wrist_angle_bk]
-"""
-def detect_pose(image, pose):
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image_rgb)
-    if results.pose_landmarks:
-        h, w, d = image.shape
-        landmarks = [(int(lm.x * w), int(lm.y * h), lm.z * w) for lm in results.pose_landmarks.landmark]
-        print(landmarks)
-        return landmarks
-    return None"""
- #-------------------------------- feedback calculation ---------------------------------------#
-ideal_angles = {"HalfMoon": [171.59, 189.31, 98.08, 94.66, 179.63, 183.02, 253.06, 106.74, 175.30, 121.65, 238.57, 309.07, 83.05, 280.71],
-                "Butterfly": [194.87, 165.46, 32.57, 32.31, 343.11, 17.54, 304.72, 52.07, 49.08, 74.14, 283.82, 305.93, 191.05, 115.79],
-                "Downward_Dog": [168.99, 166.94, 174.86, 187.43, 178.0, 178.97, 310.85, 49.03, 111.66, 81.51, 80.85, 206.01, 80.67, 79.9],
-                "Dancer": [173.19, 182.48, 127.04, 109.22, 146.81, 237.03, 202.31, 156.94, 203.75, 128.35, 238.7, 281.3, 98.15, 298.18],
-                "Triangle": [169.49, 187.7, 87.46, 114.49, 178.95, 176.47, 283.95, 85.0, 182.42, 54.19, 146.7, 326.85, 94.41, 174.23],
-                "Goddess": [166.89, 192.5, 91.66, 86.3, 242.31, 116.53, 261.62, 98.53, 151.96, 111.47, 249.03, 295.76, 131.99, 229.02],
-                "Warrior": [178.93, 179.94, 104.23, 104.73, 209.99, 143.94, 254.51, 105.94, 182.47, 115.43, 246.03, 299.39, 93.24, 264.85],
-                "Tree": [210.82, 146.19, 124.92, 119.88, 247.84, 128.14, 242.33, 162.3, 197.31, 150.54, 202.66, 289.07, 192.77, 169.7]}
+            left_knee_angle, right_knee_angle, angle_for_half_moon1, angle_for_half_moon2,
+             left_hip_angle, right_hip_angle, neck_angle_uk, left_wrist_angle_bk, right_wrist_angle_bk]
 
-thresholds_good = [10, 10, 15, 15, 10, 10, 20, 20, 15, 15, 15, 10, 20, 20] 
-thresholds_warn = [20, 20, 25, 25, 20, 20, 30, 30, 25, 25, 25, 20, 30, 30]
+ #--------------------------------same as dataset creator functions---------------------------------------#
+
+
+ideal_angles = {"HalfMoon": [171.59, 189.31, 98.08, 94.66, 179.63, 183.02, 253.06, 106.74,  121.65, 238.57, 309.07, 83.05, 280.71],
+                "Butterfly": [194.87, 165.46, 32.57, 32.31, 343.11, 17.54, 304.72, 52.07,  74.14, 283.82, 305.93, 191.05, 115.79],
+                "Downward_Dog": [168.99, 166.94, 174.86, 187.43, 178.0, 178.97, 310.85, 49.03,  81.51, 80.85, 206.01, 80.67, 79.9],
+                "Dancer": [173.19, 182.48, 127.04, 109.22, 146.81, 237.03, 202.31, 156.94,  128.35, 238.7, 281.3, 98.15, 298.18],
+                "Triangle": [169.49, 187.7, 87.46, 114.49, 178.95, 176.47, 283.95, 85.0,  54.19, 146.7, 326.85, 94.41, 174.23],
+                "Goddess": [166.89, 192.5, 91.66, 86.3, 242.31, 116.53, 261.62, 98.53,  111.47, 249.03, 295.76, 131.99, 229.02],
+                "Warrior": [178.93, 179.94, 104.23, 104.73, 209.99, 143.94, 254.51, 105.94,  115.43, 246.03, 299.39, 93.24, 264.85],
+                "Tree": [210.82, 146.19, 124.92, 119.88, 247.84, 128.14, 242.33, 162.3,  150.54, 202.66, 289.07, 192.77, 169.7]}
+
+thresholds_good = [10, 10, 15, 15, 10, 10, 20, 20,  15, 15, 10, 20, 20] 
+thresholds_warn = [20, 20, 25, 25, 20, 20, 30, 30,  25, 25, 20, 30, 30]
 ''' ADAPT VALUES '''
 
-user_angles = angles_finder(landmarks) 
+
 
 
 def compare_angles(user_angles, ideal_angles, threshold_good, threshold_warn):
@@ -142,6 +133,6 @@ while cam.isOpened():
 
 
 cam.release()
-
+pose.close()
 cv2.destroyAllWindows()
 
